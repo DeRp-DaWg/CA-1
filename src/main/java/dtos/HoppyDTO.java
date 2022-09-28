9a0ab3362a6dc47e51291dbb064b319fcb400439
Arsim Ljoki <113034331+ArsimBoy420@users.noreply.github.com>
2022-09-28 12:43:53 +0200

    ,.

---
+++ src/main/java/dtos/HoppyDTO.java
@@ -0,0 +1,40 @@
+package dtos;
+
+import entities.Hoppy;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HoppyDTO {
+    private String hoppy_name;
+    private String hoppy_description;
+
+    public HoppyDTO(String hoppy_name, String hoppy_description){
+    this.hoppy_name = hoppy_name;
+    this.hoppy_description = hoppy_description;
+    }
+
+    public static List<HoppyDTO> getDtos(List<Hoppy> hoppies){
+        List<HoppyDTO> hoppyDTOS = new ArrayList();
+        hoppies.forEach(hoppy->hoppyDTOS.add(new HoppyDTO(hoppy)));
+        return hoppyDTOS;
+    }
+    public HoppyDTO(Hoppy hoppy) {
+        if(hoppy.getHoppy_name != null)
+            this.hoppy_name = hoppy.getName();
+            this.hoppy_description = hoppy_description.getHoppy_description();
+    }
+
+    public String getHoppy_name(){return hoppy_name; }
+    public void setHoppy_name(String hoppy_name){this.hoppy_name = hoppy_name;}
+
+    public String getHoppy_description(){return hoppy_description;}
+    public void  setHoppy_description(String hoppy_description){this.hoppy_description = hoppy_description; }
+
+    @Override
+    public String toString() {
+        return "HoppyDTO{" +
+                "hoppy_name='" + hoppy_name + '\'' +
+                ", hoppy_description='" + hoppy_description + '\'' +
+                '}';
+    }
+}
